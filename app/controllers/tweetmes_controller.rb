69e3754079ec45a371c14eef6f5dc3e8516a5c21
hitomi numari <h.h.hitomi1216@gmail.com>
2020-02-13 23:03:06 +0900

    create post-related function

--- app/controllers/tweetmes_controller.rb
+++ app/controllers/tweetmes_controller.rb
@@ -1,5 +1,30 @@
 class TweetmesController < ApplicationController
+  
   def index
     @tweetmes = Tweetme.all
+  end
+
+  def new
+    @tweetme = Tweetme.new
+  end
+
+  def create
+    @tweetme = Tweetme.new(tweetme_params)
+
+    if params[:back]
+          render :new
+    else
+      if @tweetme.save
+        redirect_to tweetmes_path, notice: "New tweet has created！"
+      else
+        render 'new'
+      end
+    end
+  end
+
+private
+
+  def tweetme_params
+    params.require(:tweetme).permit(:content)
   end
 end
